380afc755d51a7de6387f54755d4fc7a9d411d9d
Santiago Figueroa <santiagofigueroavasquez@gmail.com>
2026-03-16 17:17:43 -0400

    fix(user): :fire: removing unnecessary code in user controller

--- src/main/java/com/santiago/hexagonal/modules/user/infrastructure/UserController.java
+++ src/main/java/com/santiago/hexagonal/modules/user/infrastructure/UserController.java
@@ -2,23 +2,11 @@ package com.santiago.hexagonal.modules.user.infrastructure;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import com.santiago.hexagonal.modules.user.application.usecase.TestUseCase;
 
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
-
-    @Autowired
-    private TestUseCase testUseCase;
-
-    @GetMapping
-    public String getMethodName() {
-        testUseCase.execute(null);
-        return "Hello World";
-    }
 
 }
