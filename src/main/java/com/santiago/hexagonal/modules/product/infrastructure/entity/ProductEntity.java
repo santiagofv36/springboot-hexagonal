0b0a3da39de4a99d3889eebd91723722c21f59d8
Santiago Figueroa <santiagofigueroavasquez@gmail.com>
2026-03-16 21:09:28 -0400

    feat(product): :sparkles: adding product entity

---
+++ src/main/java/com/santiago/hexagonal/modules/product/infrastructure/entity/ProductEntity.java
@@ -0,0 +1,31 @@
+package com.santiago.hexagonal.modules.product.infrastructure.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "sku", nullable = false)
+    private String sku;
+}
