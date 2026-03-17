273b9c3b9844ec3b86ef697b839686337c34abee
Santiago Figueroa <santiagofigueroavasquez@gmail.com>
2026-03-16 22:16:54 -0400

    feat(ware): :sparkles: adding warehouse entity

---
+++ src/main/java/com/santiago/hexagonal/modules/warehouse/infrastructure/entity/WarehouseEntity.java
@@ -0,0 +1,30 @@
+package com.santiago.hexagonal.modules.warehouse.infrastructure.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "warehouses")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class WarehouseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+    @Column(name = "location", nullable = false)
+    private String location;
+
+}
